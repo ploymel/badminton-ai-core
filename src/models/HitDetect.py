de8357592ca74c3156010b6331a2faa85e358013
ploymel <ploy_mel@hotmail.com>
2024-03-02 10:36:49 +0700

    add scorebox training code

--- src/models/HitDetect.py
+++ src/models/HitDetect.py
@@ -1,8 +1,13 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 
 class HitModel(nn.Module):
+    """
+    Hit detector model
+    """
+
     def __init__(self, feature_dim, num_consecutive_frames):
         super(HitModel, self).__init__()
         self.num_consecutive_frames = num_consecutive_frames
@@ -42,4 +47,28 @@ class HitDetector(object):
     """
 
     def __init__(self):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.num_consecutive_frames = 12
+        self.normalization = True
+        self.fps = 30
+
+        # for optimization
+        self.optim_num_consecutive_frames = 5
+
+        # for storing the prediction result
+        self.preds = []
+        self.hit_frames = {}
+
+    def reset(self):
+        self.preds = []
+        self.hit_frames = {}
+
+    def setup_HitDetect(self):
+        self.__hitdetect = torch.load("src/models/weights/hitdetect.pth")
+        self.__hitdetect.to(self.device).eval()
+
+    def del_HitDetect(self):
+        del self.__hitdetect
+
+    def get_hits_event(self, top_kp, bottom_kp, court_kp, ball_pos):
         pass
